85ed2168a5bc2572457739fd790ae58ecaa51569
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-13 17:11:11 +0300

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (event) => {
+    event.preventDefault()
+    makeMyToDo(document.querySelector("#new-task-description").value)
+    form.reset()
+  })
+})
+function makeMyToDo(todo) {
+  const p = document.createElement('p')
+  const btn = document.createElement('button')
+  btn.addEventListener('click', handleTheDelete)
+  btn.textContent = ' x'
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector("#list").appendChild(p)
+}
+function handleTheDelete(event){
+  event.target.parentNode.remove()
+}
